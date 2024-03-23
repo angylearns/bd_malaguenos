834f203a9f079d25097a8f16488aafd06dfd19ff
Ángela Machado <99359053+angylearns@users.noreply.github.com>
2024-03-23 23:00:02 +0100

    Create POST method

--- src/routes/ProductoRouter.py
+++ src/routes/ProductoRouter.py
@@ -1,14 +1,31 @@
 from flask import Blueprint, request
 from src.services.ProductoService import ProductoService
 
+from src.models.productoModel import Producto
+
 main = Blueprint('producto_blu', __name__)
 
 # ruta raíz
-@main.route('/')
-def get_productos():
-    print(request)
-    print(request.method)
-    ProductoService.get_productos()
+@main.route('/', methods=['GET', 'POST'])
+def manage_productos():
+    # capturamos los datos del json (1)
+    idProd = None
+    nombreProd = request.json['nombreProd']
+    descripcionProd = request.json['descripcionProd']
+    marcaProd = request.json['marcaProd']
+    precioProd = request.json['precioProd']
+    stockProd = request.json['stockProd']
 
-    print('Esto se imprime en consola')
+    # instanciamos el objeto (2)
+    nuevoProducto = Producto(0, nombreProd, descripcionProd, marcaProd, precioProd, stockProd)
+
+    if request.method == 'GET':
+        ProductoService.get_productos()
+        print("Productos:", ProductoService.get_productos())
+
+    # le pasamos como parámetro el nuevoProducto al método post_productos() (3)
+    elif request.method == 'POST':  
+        ProductoService.post_productos(nuevoProducto)
+        print('Producto insertado')
+    
     return 'Esto se ve en la página'
--- src/services/ProductoService.py
+++ src/services/ProductoService.py
@@ -1,11 +1,14 @@
 from src.database.db_mysql import get_connection
 
+from src.models.productoModel import Producto
+
 class ProductoService():
 
     @classmethod
     def get_productos(cls):
         try:
             connection = get_connection()
+
             if connection:
                 print("Conexión establecida:", connection)
                 with connection.cursor() as bd_malaguenos:
@@ -20,3 +23,28 @@ class ProductoService():
                 print("No se pudo establecer la conexión.")
         except Exception as ex:
             print("Error al obtener la conexión:", ex)
+    
+    @classmethod
+    def post_productos(cls, nuevoProducto: Producto):
+        try:
+            connection = get_connection()
+            idProd = None
+            nombreProd = nuevoProducto._nombre
+            descripcionProd = nuevoProducto._descripcion
+            marcaProd = nuevoProducto._marca
+            precioProd = nuevoProducto._precio
+            stockProd = nuevoProducto._stock
+
+            if connection:
+
+                print("Método post", connection)
+                with connection.cursor() as bd_malaguenos:
+                    bd_malaguenos.execute("INSERT INTO producto (id_producto, nombre, descripcion, marca, precio, stock) VALUES (%s, %s, %s, %s, %s, %s)", (idProd, nombreProd, descripcionProd, marcaProd, precioProd, stockProd))
+                    connection.commit()
+                connection.close()
+                return 'Lista mostrada.'
+            else:
+                print("No se pudo establecer la conexión.")
+        except Exception as ex:
+            print("Error al obtener la aplicación:", ex)
+            
