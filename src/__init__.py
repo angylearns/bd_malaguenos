884ebafa4abc125fc25a4569340f9ec047690665
Ángela Machado <99359053+angylearns@users.noreply.github.com>
2024-04-02 13:40:42 +0200

    Modify methods

--- src/__init__.py
+++ src/__init__.py
@@ -7,6 +7,8 @@ app = Flask(__name__)
 def init_app(conf):
     app.config.from_object(conf)
 
-    app.register_blueprint(ProductoRouter.main1, url_prefix='/producto')
-    app.register_blueprint(ProductoRouter.main2, url_prefix='/producto/')
+    app.register_blueprint(ProductoRouter.getR, url_prefix='/get')
+    app.register_blueprint(ProductoRouter.postR, url_prefix='/post')
+    app.register_blueprint(ProductoRouter.patchR, url_prefix='/patch/')
+    app.register_blueprint(ProductoRouter.deleteR, url_prefix='/delete/')
     return app
--- src/routes/ProductoRouter.py
+++ src/routes/ProductoRouter.py
@@ -1,14 +1,27 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, jsonify
 from src.services.ProductoService import ProductoService
-
 from src.models.productoModel import Producto
 
-main1 = Blueprint('producto_blu1', __name__)
-main2 = Blueprint('producto_blu2', __name__)
+getR = Blueprint('producto_blu_get', __name__)
+postR = Blueprint('producto_blu_post', __name__)
+patchR = Blueprint('producto_blu_patch', __name__)
+deleteR = Blueprint('producto_blu_delete', __name__)
 
-# ruta raíz
-@main1.route('/', methods=['GET', 'POST'])
-def manage_productos():
+@getR.route('/', methods=['GET'])
+def manage_productos_getR():
+    if not request.is_json:
+        return jsonify({"error": "Request must be JSON"}), 400
+
+    ProductoService.get_productos()
+    print("Productos obtenidos:", ProductoService.get_productos())
+
+    return 'Productos obtenidos.'
+
+@postR.route('/', methods=['POST'])
+def manage_productos_postR():
+    if not request.is_json:
+        return jsonify({"error": "Request must be JSON"}), 400
+    
     # capturamos los datos del json (1)
     idProd = request.json['idProd'] if 'idProd' in request.json else None
     nombreProd = request.json['nombreProd'] if 'nombreProd' in request.json else None
@@ -16,22 +29,20 @@ def manage_productos():
     marcaProd = request.json['marcaProd'] if 'marcaProd' in request.json else None
     precioProd = request.json['precioProd'] if 'precioProd' in request.json else None
     stockProd = request.json['stockProd'] if 'stockProd' in request.json else None
-
-
+    
     # para nuevo producto, instanciamos el objeto del nuevo producto (2)
     nuevoProducto = Producto(0, nombreProd, descripcionProd, marcaProd, precioProd, stockProd)
 
-    if request.method == 'GET':
-        ProductoService.get_productos()
-        print("Productos:", ProductoService.get_productos())
-
     # le pasamos como parámetro el nuevoProducto al método post_productos() (3)
-    elif request.method == 'POST':  
-        ProductoService.post_productos(nuevoProducto)
-        print('Producto insertado')
+    ProductoService.post_productos(nuevoProducto)
+    print('Producto insertado: ', nuevoProducto)
 
-@main2.route('/<int:id_producto>', methods=['PATCH', 'DELETE'])
-def manage_productos(id_producto):
+    return 'Producto creado.'
+
+@patchR.route('/<int:id_producto>', methods=['PATCH'])
+def manage_productos_postR(id_producto):
+    if not request.is_json:
+        return jsonify({"error": "Request must be JSON"}), 400
     # capturamos los datos del json (1)
     idProd = request.json['idProd'] if 'idProd' in request.json else None
     nombreProd = request.json['nombreProd'] if 'nombreProd' in request.json else None
@@ -43,12 +54,16 @@ def manage_productos(id_producto):
     # Instanciamos el objeto productoActualizado con los datos capturados
     productoActualizado = Producto(idProd, nombreProd, descripcionProd, marcaProd, precioProd, stockProd)
 
-    if request.method == 'PATCH':
-        ProductoService.update_productos(productoActualizado)
-        print('Producto actualizado', productoActualizado)
+    ProductoService.update_productos(productoActualizado)
+    print('Producto actualizado: ', productoActualizado)
 
-    elif request.method == 'DELETE':
-        ProductoService.delete_productos(idProd) 
-        print('Producto eliminado')
-        
-    return 'Esto se ve en la página'
+    return 'Producto actualizado.'
+
+@deleteR.route('/<int:idProd>', methods=['DELETE'])
+def manage_productos_deleteR(idProd):
+    if not request.is_json: 
+        return jsonify({"error": "Request must be JSON"}), 400
+    ProductoService.delete_productos(idProd) 
+    print('Producto eliminado.')
+
+    return 'Producto eliminado.'
